264672abaaff7b6da3b956fc11b240c0a17a8526
Heiler Nova <heilernova@outlook.com>
2025-02-15 14:30:55 -0500

    feat(website): agrega componente layout

--- apps/website/src/app/app.routes.ts
+++ apps/website/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    { path: '', loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent) },
+];
---
+++ apps/website/src/app/layout/layout.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LayoutComponent {
+
+}
