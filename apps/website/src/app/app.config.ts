43def2470a41d4cf9d386f6db7b9153ef464e643
Heiler Nova <heilernova@outlook.com>
2025-02-15 15:23:55 -0500

    feat(website): configurar ng zorro

--- apps/website/src/app/app.config.ts
+++ apps/website/src/app/app.config.ts
@@ -1,15 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import es from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNzI18n(es_ES),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: "es" }
   ],
 };
--- apps/website/src/app/layout/layout.component.ts
+++ apps/website/src/app/layout/layout.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { NzButtonModule } from 'ng-zorro-antd/button';
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [
+    NzButtonModule,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
