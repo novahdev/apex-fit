82ddb405d2a5cf4c41ed7c67f21422ae91c539ef
Heiler Nova <heilernova@outlook.com>
2025-02-22 12:40:55 -0500

    feat(website): confirmar la carga de la sesión

--- apps/website/src/app/app.component.ts
+++ apps/website/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from './auth';
 
 @Component({
   imports: [RouterModule],
@@ -9,4 +10,9 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'website';
+  private readonly _authService: AuthService = inject(AuthService);
+
+  constructor(){
+    this._authService.init();
+  }
 }
--- apps/website/src/app/layout/layout.component.ts
+++ apps/website/src/app/layout/layout.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { AuthService } from '../auth';
 @Component({
   selector: 'app-layout',
   imports: [
@@ -13,5 +14,18 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent {
+  private readonly _authService: AuthService = inject(AuthService);
+  protected readonly isLoggedIn = signal<boolean>(false);
+  protected readonly name = signal<string>('');
 
+  constructor(){
+    this.isLoggedIn.set(this._authService.isLoggedIn);
+    this.name.set(this._authService.session?.user.name ?? 'Guest');
+    this._authService.sessionChange().subscribe((session) => {
+      this.isLoggedIn.set(session !== null);
+      if (session) {
+        this.name.set(session.user.name);
+      }
+    });
+  }
 }
