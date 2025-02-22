f41ae5550a8bcadd2bc80f8d357382d9780dbc1b
Heiler Nova <heilernova@outlook.com>
2025-02-22 10:53:29 -0500

    chore(api): mapear los datos de los usuarios al mementos de crear o editar información

--- apps/api/src/app/models/users/users.service.ts
+++ apps/api/src/app/models/users/users.service.ts
@@ -2,8 +2,10 @@ import { DbConnectionService } from '@app/api/common/db-connection';
 import { Injectable } from '@nestjs/common';
 import { FIELDS_USERS } from './queries';
 import { UserCreatePayload, UserRaw } from '@app/shared/schemas/users';
+import { capitalize } from '@app/shared/utils/capitalize';
 import { User } from './user.model';
 import { isEmail, isUUID } from 'class-validator';
+import { hash } from 'argon2';
 
 @Injectable()
 export class UsersService {
@@ -16,17 +18,25 @@ export class UsersService {
         return user ? new User(user) : undefined;
     }
 
-    public async getAllUsers(){
+    public async getAllUsers(): Promise<UserRaw[]> {
         const users = (await this._db.query<UserRaw>(`SELECT ${FIELDS_USERS} FROM users`)).rows;
         return users;
     }
 
     public async createUser(data: UserCreatePayload): Promise<User>{
+        data.name = capitalize(data.name);
+        data.lastName = capitalize(data.lastName);
+        data.email = data.email.toLowerCase();
+        data.password = await hash(data.password);
         const user = (await this._db.insert<UserRaw>("users", data, FIELDS_USERS)).rows[0];
         return new User(user);
     }
 
     public async updateUser(id: string, data: Partial<UserCreatePayload>): Promise<UserRaw>{
+        if (data.name) data.name = capitalize(data.name);
+        if (data.email) data.email = data.email.toLowerCase();
+        if (data.lastName) data.lastName = capitalize(data.lastName);
+        if (data.password) data.password = await hash(data.password);
         const user = (await this._db.update<UserRaw>("users", ["id = $1", [id]], data, FIELDS_USERS)).rows[0];
         return user;
     }
