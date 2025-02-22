ad019206a775d598d40f732221768ebab16adc5a
Heiler Nova <heilernova@outlook.com>
2025-02-22 15:46:25 -0500

    chore(shared): agrega interfaz para la datos de auth

--- packages/shared/src/api/auth/api-auth-interfaces.ts
+++ packages/shared/src/api/auth/api-auth-interfaces.ts
@@ -1,20 +1,22 @@
 import { UserRole } from "@app/shared/schemas/users/users.interfaces";
 import { ApiResponse } from "../global/api-response.interface";
 
-export interface ApiAuthResponse extends ApiResponse {
-    data: {
-        accessToken: string;
-        refreshToken: string;
-        user: {
-            id: string;
-            role: UserRole;
-            alias: string | null;
-            isCoach: boolean;
-        }
-        gym?: {
-            id: string;
-            name: string;
-            slug: string;
-        }
+export interface ApiAuthData {
+    accessToken: string;
+    refreshToken: string;
+    user: {
+        id: string;
+        role: UserRole;
+        name: string;
+        isCoach: boolean;
     }
+    gym?: {
+        id: string;
+        name: string;
+        slug: string;
+    }
+}
+
+export interface ApiAuthResponse extends ApiResponse {
+    data: ApiAuthData;
 }
