8ca22c4215e214f3f3f881c46958df1d5fb1d334
Heiler Nova <heilernova@outlook.com>
2025-02-22 10:58:40 -0500

    chore(website): manejamos los errores 401

--- apps/website/src/app/config/api.interceptor.ts
+++ apps/website/src/app/config/api.interceptor.ts
@@ -1,16 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const url = `api/${req.url}`;
+  const router = inject(Router);
+  const nzMessage = inject(NzMessageService);
   
 
   return next(req.clone({ url })).pipe(
     catchError(err => {
       if (err.status === 401) {
-        console.log("Error")
+        router.navigate(['/']);
+        nzMessage.error('Tú sesión ha expirado');
       }
       return throwError(() =>err);
     })
